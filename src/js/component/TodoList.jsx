c35f8ed3859f755cca35e5f5679c8b9f53f42609
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-10-26 22:53:11 +0000

    everything done

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -16,14 +16,22 @@ const TodoList = () => {
         }
     };
 
+    const handleDeleteTodo = (index) => {
+        const updateTodos = [...todos];
+        updateTodos.splice(index, 1);
+        setTodos(updateTodos);
+    }
+
     return (
         <>
             <h1 className='text-center'>todos</h1>
             <div className="note">
                 <ul>
-                    <li><input type="text" onChange={handleInputChange} onKeyUp={handleInputKeyPress} value={todoInput} placeholder="What needs to be done?"></input></li>
+                    <li><input className="inputy" type="text" onChange={handleInputChange} onKeyUp={handleInputKeyPress} value={todoInput} placeholder="What needs to be done?"></input></li>
                     {todos.map((todo, index) => (
-                        <li key={index}>{todo}</li>
+                        <li key={index}>{todo}
+                            <button className="trash" onClick={() => handleDeleteTodo(index)}><i class="fa-solid fa-trash-can"></i></button>
+                        </li>
                     ))}
                 </ul>
             </div>
